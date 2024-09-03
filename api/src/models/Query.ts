be021eaa1b28bc9e9800706a29a18f7cc5995d2c
Caio Lucena Colaço <caio.lcolaco@gmail.com>
2024-09-03 20:42:02 -0300

    solving bugs

--- api/src/models/Query.ts
+++ api/src/models/Query.ts
@@ -10,12 +10,19 @@ export class Query {
     const result: string[] = [];
     let cost: number = 0;
 
+    let found = false
     for (const page of pages) {
-      cost++;
-      for (const pageItem of page.items) {
-        result.push(pageItem);
-        if (pageItem === item) break;
-      }
+        cost++
+        for (const pageItem of page.items) {
+            result.push(pageItem);
+            if (pageItem === item) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            break;
+        }
     }
 
     return { items: result, cost };
