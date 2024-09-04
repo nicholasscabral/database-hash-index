edefa8d874dbdd323b378f8b18ceb2c8f6568aed
Caio Lucena Colaço <caio.lcolaco@gmail.com>
2024-09-04 09:02:18 -0300

    feature: Added the hash search

--- api/src/models/Query.ts
+++ api/src/models/Query.ts
@@ -1,6 +1,13 @@
-import { pages } from "../routes";
+import { buckets, pages } from "../routes";
+import { hash } from "../utils/hash";
+import { BUCKET_SIZE } from "./bucket";
 
 interface TableScanResult {
+  items: string[];
+  cost: number;
+}
+
+interface hashSearchResult {
   items: string[];
   cost: number;
 }
@@ -23,6 +30,36 @@ export class Query {
         if (found) {
             break;
         }
+    }
+
+    return { items: result, cost };
+  }
+
+  static hashSearch(item: string): hashSearchResult {
+    const numBuckets: number = Math.ceil(
+      pages.reduce((a, c) => a + c.items.length, 0) / BUCKET_SIZE
+    );
+    const hashWord = hash(item, numBuckets)
+
+    const findedBucket = buckets[hashWord]
+    let indexPage = 0
+    for (let ref of findedBucket.refs) {
+      let tupla = ref.find(obj => obj.hasOwnProperty(item))
+      if (tupla) {
+        indexPage = tupla[item]
+        break
+      }
+    }
+
+    const findedPage = pages[indexPage]
+    const result: string[] = [];
+    let cost: number = 1;
+
+    for(let word of findedPage.items) {
+      result.push(word)
+      if (item === word) {
+        break;
+      }
     }
 
     return { items: result, cost };
